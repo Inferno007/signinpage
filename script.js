0d400d95740a5600466a3632ecf8675f8feda135
satyaraj07 <62071669+satyaraj07@users.noreply.github.com>
2020-04-26 14:03:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+document.querySelector("#auth-signup").addEventListener("click",active1);
+document.querySelector("#auth-login").addEventListener("click",active2);
+function active1(){
+	var tag=document.querySelectorAll(".p");
+	var x=tag[0].getAttribute("aria-selected");
+	var y=tag[1].getAttribute("aria-selected");
+	console.log(x)
+	console.log(y)
+	if (y==true){
+		tag[1].setAttribute("aria-selected","false");
+		tag[1].classList.remove("active");
+	}
+	if (x==false){
+		tag[0].setAttribute("aria-selected","true");
+		tag[0].classList.add("active");
+	}
+}
+function active2(){
+	var tag=document.querySelectorAll(".p");
+	var x=tag[0].getAttribute("aria-selected");
+	var y=tag[1].getAttribute("aria-selected");
+	if (x==true){
+		tag[0].setAttribute("aria-selected","false");
+		tag[0].classList.remove("active");
+	}
+	if (y==false){
+		tag[1].setAttribute("aria-selected","true");
+		tag[1].classList.add("active");
+	}
+}
+
+
+
+
