4a8b80fd688cebce89e432a1c7fc93a1473bdb81
satyaraj07 <62071669+satyaraj07@users.noreply.github.com>
2020-04-26 18:01:11 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-document.querySelector("#auth-signup").addEventListener("click",active1);
-document.querySelector("#auth-login").addEventListener("click",active2);
+window.onload=function(){
+  	document.querySelector("#auth-signup").addEventListener("click",active1);
+	document.querySelector("#auth-login").addEventListener("click",active2);
+}
+
 function active1(){
 	var tag=document.querySelectorAll(".p");
 	var x=tag[0].getAttribute("aria-selected");
 	var y=tag[1].getAttribute("aria-selected");
 	console.log(x)
 	console.log(y)
-	if (y==true){
+	if (y=='true'){
 		tag[1].setAttribute("aria-selected","false");
 		tag[1].classList.remove("active");
 	}
-	if (x==false){
+	if (x=='false'){
 		tag[0].setAttribute("aria-selected","true");
 		tag[0].classList.add("active");
 	}
@@ -19,16 +22,13 @@ function active2(){
 	var tag=document.querySelectorAll(".p");
 	var x=tag[0].getAttribute("aria-selected");
 	var y=tag[1].getAttribute("aria-selected");
-	if (x==true){
+	if (x=='true'){
 		tag[0].setAttribute("aria-selected","false");
 		tag[0].classList.remove("active");
 	}
-	if (y==false){
+	if (y=='false'){
 		tag[1].setAttribute("aria-selected","true");
 		tag[1].classList.add("active");
 	}
 }
-
-
-
 
